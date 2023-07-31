0700d41e981ba8ad56c82f2d03abaf5bebf96e46
rogue <throwaway>
2023-07-31 18:49:25 +0000

    use blank string substitute for git prompt when not in a git directory

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -15,11 +15,15 @@ function fish_prompt
   if fish_is_root_user
       set user_char '#'
   end
+  set -l git_prompt (fish_git_prompt)
+  if test "$git_prompt" = ""
+    set git_prompt ''
+  end
   printf '%s%s%s@%s%s %s%s%s%s%s\n[%s] '    \
       (set_color brmagenta) $USER           \
       (set_color normal)                    \
       (set_color brblue) $hostname          \
       (set_color bryellow) (pwd)            \
-      (set_color magenta) (fish_git_prompt) \
+      (set_color magenta)  $git_prompt \
       (set_color normal) $user_char
 end
