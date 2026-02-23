539755489ef2d4869498b72a3f62fcfb8dbc026a
u16rogue <u16rogue@ushort.moe>
2026-02-23 14:49:20 +0800

    rework `tmuxss` user script

--- users/user/scripts/default.nix
+++ users/user/scripts/default.nix
@@ -1,4 +1,21 @@
 { pkgs, ... }: [
     (pkgs.writeShellScriptBin "mkignore" (builtins.readFile ./mkignore))
-    (pkgs.writeShellScriptBin "tmuxss" (builtins.readFile ./tmuxss))
+    (pkgs.writeShellScriptBin "tmuxss" /*bash*/ ''
+        if [ -z "$1" ]; then
+            echo "Missing directory. Usage: $0 <directory>"
+            exit 1
+        fi
+        
+        SESSION_DIR=$(${pkgs.coreutils}/bin/realpath "$1")
+        if [ ! -d "$SESSION_DIR" ]; then
+            echo "Invalid directory. Not found: $SESSION_DIR"
+            exit 1
+        fi
+        
+        SESSION_NAME=$(${pkgs.coreutils}/bin/basename "$1")
+        
+        # Session
+        ${pkgs.tmux}/bin/tmux new-session -d -s "$SESSION_NAME" -A -c "$SESSION_DIR"
+        echo "new session created"
+    '')
 ]
